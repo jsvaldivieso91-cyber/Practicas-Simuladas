29647f197ad2d474f68d97bdc41a76731d03248a
jsvaldivieso91-cyber <jsvaldivieso91@gmail.com>
2026-05-04 20:43:28 -0500

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,3 +1,13 @@
+"""
+Módulo encargado del registro de eventos y errores del sistema.
+"""
+
 def registrar_log(mensaje):
+    """
+    Guarda un mensaje en el archivo logs.txt.
+
+    Args:
+        mensaje (str): Mensaje a registrar
+    """
     with open("logs.txt", "a") as f:
         f.write(mensaje + "\n")
